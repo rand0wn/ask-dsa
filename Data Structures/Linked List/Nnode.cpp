33f723233e9c3bdd3be29391d1f4a640116925b1
rand0wn <abhishek18kushwah@gmail.com>
2015-10-30 11:52:51 +0530

    Fixed end nodes algorithm

--- Data Structures/Linked List/Nnode.cpp
+++ Data Structures/Linked List/Nnode.cpp
@@ -2,40 +2,52 @@
 
 #include<iostream>
 #include<forward_list>
+#include<iterator>
 
 //Using iteration
 
 int iReturn(std::forward_list<int> l, int n){
 
-	int k;
-	for(auto i = l.begin(); i <= (l.end() - n + 1); i++)
-		k = *i;
+	int k = 0;
 
-	return k;
+	for(auto i = l.begin(); i != l.end(); ++i){
+		if(++k == std::distance(l.begin(), l.end()) - n + 1)
+			return *i;
+	}
 }
 
-/*
+
 //Using recursion
 int rReturn(std::forward_list<int> l, int n){
 
     static int i = 0;
-    auto it = l.begin();
+
+	if(i == n)
+ 	return l.front();
+
+	if(!l.empty())
+		
+	{
+	int a = l.front();
 	
-	if(it != l.end())
-		return;
-	rReturn(++it, n);
+	l.pop_front();
+
+	rReturn(l, n);
 	
-	if(++i == n)
-		return *it; 
+	l.push_front(a);
+
+	i++;
+	
+     }
+	
 }
-*/
 
 //Using two pointers
 int pReturn(std::forward_list<int> l, int n){
 
 	auto i1 = l.begin(), i2 = l.begin();
 
-	i1 += n;
+	std::advance(i1, n); //Move iterator to n places
 	
 	while(i1 != l.end()){
 		++i1;
@@ -47,8 +59,8 @@ int pReturn(std::forward_list<int> l, int n){
 
 int main(){
 	
-	std::forward_list<int> s = {1,2,3};
-	std::cout<<iReturn(s, 1)<<" "<<pReturn(s, 1)<<"\n";
+	std::forward_list<int> s = {1,2,3,4,5,6};
+	std::cout<<iReturn(s, 1)<<" "<<rReturn(s, 1)<<" "<<pReturn(s, 1)<<"\n";
 	
 	return 0;
 }
